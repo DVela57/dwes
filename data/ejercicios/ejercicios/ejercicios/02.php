3214e7f2cbad4fb764db6bfbac12a32262cb0507
DVela57 <113922101+DVela57@users.noreply.github.com>
2022-11-05 21:10:40 +0100

    e

--- data/ejercicios/ejercicios/ejercicios/02.php
+++
@@ -1,7 +0,0 @@
-<?php
-//uso den constante para poder mofificar el numero
-    const NUM = 8;
-    echo "<h1>Tabla de multiplical de ". NUM . "</h1>";
-    for ($x=0; $x <=12 ; $x++) { 
-        print "<br>" . NUM . " x " . $x . " = " . NUM * $x;
-    }
--- data/ejercicios/ejercicios/ejercicios/20/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-    session_start();
-    require_once "App.php";
-    $app = new App;
-    $app->run();
